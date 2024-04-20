8b5fc27e51f132ef92cb92df9306875b2aa24af7
Ekaterina Kishova <kishovaem@rambler.ru>
2024-04-20 20:15:02 +0400

    guess_number

--- guess_number.py
+++ guess_number.py
@@ -3,7 +3,7 @@
 from random import randint
 
 # Получаем случайное число в диапазоне от 1 до 100.
-number = randint(1, 100)знерщ
+number = randint(1, 100)
 print('Угадайте число от 1 до 100')
 
 while True:
